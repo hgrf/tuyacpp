2230e095d6fa2be06d08f53ff775a7a1cd2fb29a
Holger Graef <holger.graef@gmail.com>
2023-05-13 17:34:03 +0200

    refactor logging methods

--- logging.hpp
+++ logging.hpp
@@ -54,4 +54,15 @@ const std::map<LogStream::Level, std::string> LogStream::ColorMap{{
 const std::string LogStream::RESET_COLOR("\e[0m");
 std::map<std::string, LogStream> LogStream::mLogStreams = {};
 
+#define LOG_MEMBERS(t) \
+    virtual const std::string& TAG() { static const std::string tag = #t; return tag; }; \
+    LogStream& LOGD() { return LogStream::get(TAG(), LogStream::DEBUG); } \
+    LogStream& LOGI() { return LogStream::get(TAG(), LogStream::INFO); } \
+    LogStream& LOGW() { return LogStream::get(TAG(), LogStream::WARNING); } \
+    LogStream& LOGE() { return LogStream::get(TAG(), LogStream::ERROR); } \
+    std::ostream& EV_LOGD(Event &e) { return e.log(LogStream::get(TAG(), LogStream::DEBUG)); } \
+    std::ostream& EV_LOGI(Event &e) { return e.log(LogStream::get(TAG(), LogStream::INFO)); } \
+    std::ostream& EV_LOGW(Event &e) { return e.log(LogStream::get(TAG(), LogStream::WARNING)); } \
+    std::ostream& EV_LOGE(Event &e) { return e.log(LogStream::get(TAG(), LogStream::ERROR)); }
+
 }   // namespace tuya
--- loop/handler.hpp
+++ loop/handler.hpp
@@ -17,9 +17,6 @@ class Handler {
     static const size_t BUFFER_SIZE = 1024;
 
 public:
-    LogStream& LOGGER(LogStream::Level lev) { return LogStream::get(TAG(), lev); }
-    virtual const std::string& TAG() { static const std::string tag = "HANDLER"; return tag; };
-
     Handler(const std::string& key = DEFAULT_KEY) : mBuffer("\0", BUFFER_SIZE), mKey(key) {
     }
 
@@ -75,25 +72,7 @@ public:
     }
 
 protected:
-    LogStream& LOGD() { return LOGGER(LogStream::DEBUG); }
-    LogStream& LOGI() { return LOGGER(LogStream::INFO); }
-    LogStream& LOGW() { return LOGGER(LogStream::WARNING); }
-    LogStream& LOGE() { return LOGGER(LogStream::ERROR); }
-    std::ostream& EV_LOG(Event &e, LogStream::Level l) {
-        return e.log(LOGGER(l));
-    }
-    std::ostream& EV_LOGD(Event &e) {
-        return EV_LOG(e, LogStream::DEBUG);
-    }
-    std::ostream& EV_LOGI(Event &e) {
-        return EV_LOG(e, LogStream::INFO);
-    }
-    std::ostream& EV_LOGW(Event &e) {
-        return EV_LOG(e, LogStream::WARNING);
-    }
-    std::ostream& EV_LOGE(Event &e) {
-        return EV_LOG(e, LogStream::ERROR);
-    }
+    LOG_MEMBERS("HANDLER");
     std::unique_ptr<Message> mMsg;
 
 private:
